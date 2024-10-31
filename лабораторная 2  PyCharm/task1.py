4f5c7630d714a4c2497babaa07a5dd6b050dc87d
LizaSamoylova24 <elizaveta.samoylova.05@inbox.ru>
2024-11-01 00:00:05 +0300

    Update task1.py

--- лабораторная 2  PyCharm/task1.py
+++ лабораторная 2  PyCharm/task1.py
@@ -1,13 +1,12 @@
-money_capital = 20000  # Подушка безопасности
-salary = 5000  # Ежемесячная зарплата
-spend = 6000  # Траты за первый месяц
-increase = 0.05  # Ежемесячный рост цен
-
-# TODO Посчитайте количество  месяцев, которое можно протянуть без долгов
-months = 0  # Количество месяцев, которые можно протянуть без долгов
+money_capital = 20000
+salary = 5000
+spend = 6000
+increase = 0.05
+months = 0
 
 while money_capital + salary >= spend:
-    months += 1
-    money_capital += salary - spend  # Обновляем подушку безопасности
-    spend *= (1 + increase)           # Увеличиваем расходы на 5%
+      months += 1
+      money_capital += salary - spend
+      spend *= (1 + increase)
+
 print("Количество месяцев, которое можно протянуть без долгов:",months)
