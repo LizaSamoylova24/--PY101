bcc52829df31ee4c0c5ea2c3a5140b29a578cc03
LizaSamoylova24 <elizaveta.samoylova.05@inbox.ru>
2024-11-08 21:48:56 +0300

    Update task2.py

--- лабораторная 3 PyCharm/task2.py
+++ лабораторная 3 PyCharm/task2.py
@@ -1,7 +1,6 @@
 def find_common_participants(group1, group2, separator=','):
     participants_first_group = set(group1.split(separator))
     participants_second_group = set(group2.split(separator))
-
     common_participants = participants_first_group.intersection(participants_second_group)
 
     return sorted(common_participants)
@@ -9,7 +8,6 @@ def find_common_participants(group1, group2, separator=','):
 
 participants_first_group = "Иванов|Петров|Сидоров"
 participants_second_group = "Петров|Сидоров|Смирнов"
-
 common_participants = find_common_participants(participants_first_group, participants_second_group, separator='|')
 print(common_participants)
 
@@ -18,6 +16,7 @@ participants_second_group2 = "Петров,Сидоров,Смирнов"
 
 common_participants2 = find_common_participants(participants_first_group2, participants_second_group2, separator=',')
 print(common_participants2)
+
 
 
 
