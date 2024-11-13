85e6b0ea74550f3dee4c133ba12a244ae89e2a74
LizaSamoylova24 <elizaveta.samoylova.05@inbox.ru>
2024-11-13 17:00:23 +0300

    Update task2.py

--- лабораторная 4 PyCharm/task2.py
+++ лабораторная 4 PyCharm/task2.py
@@ -4,20 +4,21 @@ import csv
 INPUT_FILENAME = "input.csv"
 OUTPUT_FILENAME = "output.json"
 
+
 def task() -> None:
     # Чтение содержимого CSV файла
-    with open(INPUT_FILENAME, "r", encoding="utf-8") as n:
-        reader = csv.DictReader(n)
-        c = [row for row in reader]
+    with open(INPUT_FILENAME, "r", encoding="utf-8") as file:
+        reader = csv.DictReader(file)
+        data = [row for row in reader]
 
     # Сериализация в файл с отступами равными 4
-    with open(OUTPUT_FILENAME, "w", encoding="utf-8") as u:
-        json.dump(c, u, indent=4)
+    with open(OUTPUT_FILENAME, "w", encoding="utf-8") as output_file:
+        json.dump(data, output_file, indent=4)
 
 
 if __name__ == '__main__':
     task()
 
-    with open(OUTPUT_FILENAME, "r", encoding="utf-8") as output_f:
-        for line in output_f:
+    with open(OUTPUT_FILENAME, "r", encoding="utf-8") as output_file:
+        for line in output_file:
             print(line, end="")
