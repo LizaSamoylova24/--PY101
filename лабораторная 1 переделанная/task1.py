18cb121a1931a495ea3053990f4a961822af85e0
LizaSamoylova24 <elizaveta.samoylova.05@inbox.ru>
2024-11-25 00:03:38 +0300

    Update task1.py

--- лабораторная 1 переделанная/task1.py
+++ лабораторная 1 переделанная/task1.py
@@ -1,14 +1,14 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 
 # Заменить значение пропущенного элемента средним арифметическим
-In_None = 0
-Summ = 0
+in_None = 0
+summ = 0
 
 for i, number in enumerate(numbers):
     if numbers[i] is None:
-        In_None = i
+        in_None = i
     else:
-        Summ += numbers[i]
+        summ += numbers[i]
 
-numbers[In_None] = (Summ / len(numbers))
+numbers[in_None] = (summ / len(numbers))
 print("Измененный список:", numbers)
